b4973c6bf320e18efc66243c43ef009271296fca
Timothy Gelner <Timothy.Gelner@usac.loc>
2019-11-26 17:05:58 -0500

    Updates

---
+++ precache-manifest.c5b4554bb7f70609c6691673cc9c8b91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a8b6cafc5f069474f9f63f349b2b7d",
+    "url": "/rgb-idle/index.html"
+  },
+  {
+    "revision": "2e324758ba5db1f607ba",
+    "url": "/rgb-idle/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "48eceec27aad7c2c534c",
+    "url": "/rgb-idle/static/js/2.3e76b10a.chunk.js"
+  },
+  {
+    "revision": "2e324758ba5db1f607ba",
+    "url": "/rgb-idle/static/js/main.54f7527f.chunk.js"
+  },
+  {
+    "revision": "96316d1894bcd952962d",
+    "url": "/rgb-idle/static/js/runtime-main.f0654103.js"
+  }
+]);
