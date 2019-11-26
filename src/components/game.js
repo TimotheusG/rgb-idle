53fd0818f880172620356292fcc43aa0341bc006
timothy.gelner <timothy.gelner@usac.org>
2019-11-26 16:31:52 -0500

    removed the test value

--- src/components/game.js
+++ src/components/game.js
@@ -8,13 +8,7 @@ class Game extends Component {
   constructor(props){
     super(props);
 
-    var a = Math.floor( Math.random() * 255);
-    var b = Math.floor( Math.random() * 255);
-    var c = Math.floor( Math.random() * 255);
-
-    var col = new Color(a,b,c);
-
-    this.state = {name: 'abc', colors: ['#ff0000', '#00ff00', '#0000ff'], test: col.getColorString()};
+    this.state = {name: 'abc', colors: ['#ff0000', '#00ff00', '#0000ff']};
   }
   handleChange(param) {
     this.setState({name: param});
