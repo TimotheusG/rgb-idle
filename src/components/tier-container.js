baa23264ac4ca8cefb7c2d2a7a7d366d92fbf07e
timothy.gelner <timothy.gelner@usac.org>
2019-12-09 13:30:05 -0500

    removed tiers typescript

--- src/components/tier-container.js
+++ src/components/tier-container.js
@@ -2,33 +2,30 @@ import React, { Component } from 'react';
 import Game from './game'
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
-import Tier from '../models/tier'
-import Tiers from '../models/tiers'
 
 class TierContainer extends Component {   
     constructor(props){
         super(props);
-        var t = new Tiers(new Tier());    
-        this.state = {tiers: t.stack, newPixels: 0};
+        
+        this.state = {tiers: [1], newPixels: 0};
       } 
 
       upgradeTier = (colorsCount) => {
         var highestTier = this.state.tiers[this.state.tiers.length-1];
-        var newTierNumber = highestTier.tier_number + 1;
-        var newTier = new Tier(highestTier.colors.length,newTierNumber);
-        this.setState(previousState => ({ tiers:[...previousState.tiers, newTier]}));
+        var newTier = highestTier + 1;
+        this.setState(previousState => ({ tiers:[...previousState.tiers, newTier],newPixels: colorsCount}));
       };
     
   render() {
     return (
         <Tabs defaultActiveKey='1'>
         {this.state.tiers.map(tier=>
-        <Tab eventKey={tier.tier_number} title={tier.tier_name}>
-          {tier.tier_number === 1 ?
+        <Tab eventKey={tier} title={"Tier " + tier}>
+          {tier === 1 ?
           <Game upgradeTier={this.upgradeTier} default={true}></Game>
           :
           <Game upgradeTier={this.upgradeTier} default={false} pixels={this.state.newPixels}></Game>
-  }
+          }
           </Tab>
           )}        
         </Tabs>
