fb052c0df80254bf2f72cd60647e49d3ad5f6817
timothy.gelner <timothy.gelner@usac.org>
2019-12-23 16:56:01 -0500

    fixed color-cost display

--- src/components/color-container.js
+++ src/components/color-container.js
@@ -12,7 +12,7 @@ class ColorContainer extends Component {
     return (
       <div class="col">
         <div class="row">
-          <button id="color-btn" class="btn btn-primary btn-block" disabled={this.props.pixels < this.props.color_cost} onClick={this.addColor}>Add New Color (Cost: {this.props.color_cost} Pixels)</button>
+          <button id="color-btn" class="btn btn-primary btn-block" disabled={this.props[this.props.id].pixels < this.props[this.props.id].color_cost} onClick={this.addColor}>Add New Color (Cost: {this.props[this.props.id].color_cost} Pixels)</button>
         </div>
         <div class="row">
           {this.props[this.props.id].colors.map(color => <button class="btn btn-color" style={{ backgroundColor: color }}>{color}</button>)}
